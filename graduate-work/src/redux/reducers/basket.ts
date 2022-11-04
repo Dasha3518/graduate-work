49e93e233fecc86e4ab6b5a84c31869ec7c6826e
Dasha3518 <fedorovadasha3518@gmaol.com>
2022-11-04 14:17:23 +0300

    srart

--- graduate-work/src/redux/reducers/basket.ts
+++ graduate-work/src/redux/reducers/basket.ts
@@ -1,9 +1,10 @@
 import { AnyAction, Reducer } from "redux";
+import { IProducts } from "../../types/products";
 import { ACTIONS } from "../constants";
 
 
 export interface IBasketProducts {
-    selectedProducts: IBasketProducts[];
+    selectedProducts: IProducts[];
 }
 
 const defaultState: IBasketProducts  = {
@@ -16,7 +17,7 @@ export const basketReducer = (state = defaultState, action: AnyAction) =>{
     switch (action.type) {
         case ACTIONS.SELECTED_PRODUCTS:
             const products = action.products;
-            const NewSelectedProducts = state.selectedProducts.concat([products]);
+            const NewSelectedProducts = state.selectedProducts.concat(products);
 
             return{
                 selectedProducts: NewSelectedProducts,
